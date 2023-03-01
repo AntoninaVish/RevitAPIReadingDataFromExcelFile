7176644ed09434b9a4bd18a56f9acb286e2f778f
AntoninaVish <vishivtonya@gmail.com>
2023-03-01 20:42:59 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,104 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevitAPIReadingDataFromExcelFile
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            //вызывается диалоговое окно
+            OpenFileDialog openFileDialog1 = new OpenFileDialog // создаем переменную
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop), // задается директория по умолчанию, которая будет открываться при выводе окна
+                Filter = "Excel files (*.xlsx) | *.xlsx" // указывается фильтр, указали фильтр только для файла Excel
+            };
+
+            string filePath = string.Empty;
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) // если все прошло удачно
+            {
+                filePath = openFileDialog1.FileName; //то сохраняем путь к файлу в переменную filePath
+            }
+
+            if (string.IsNullOrEmpty(filePath)) //если путь не указан 
+                return Result.Cancelled; //возвращаем отмену
+
+            //собираем все помещения с которыми будем работать
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+
+            //мы должны открыть по указанному пути файл Excel и прочитать из него данные, т.е открываем файл не создаем новый и открываем этот файл для чтения
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                IWorkbook workbook = new XSSFWorkbook(filePath);//создаем переменную для чтения файла Excel, указываем ему путь
+                ISheet sheet = workbook.GetSheetAt(index: 0); //берем лист с которого будем читать данные, будет самый первый лист в файле
+
+                //будем проходить построчно файла Excel и собирать данные, для этого создаем целочисленную переменную
+                int rowIndex = 0;
+
+                //используем цикл while, в строке есть данные, она существует, то будем продолжать данный цикл, когда данных не будет цикл закончится
+                while (sheet.GetRow(rowIndex) != null)
+                {
+                    //если в указаной строке в первом столбце GetCell(0) ячейка пустая
+                    //либо ячейка пустая во втором столбце GetCell(1), то мы переходим к следующей операции continue
+                    if (sheet.GetRow(rowIndex).GetCell(0) == null ||
+                        sheet.GetRow(rowIndex).GetCell(1) == null)
+                    {
+                        //когда мы переходим к следующей операции должны увеличивать rowIndex, поэтому нужно это учитывать, чтобы не попасть в вечный цикл
+                        rowIndex++;
+                        continue;
+                    }
+                      
+
+                    //считываем данные из файл Excel: берем лист, берем строку 
+                    string name = sheet.GetRow(rowIndex).GetCell(0).StringCellValue;
+                    string number = sheet.GetRow(rowIndex).GetCell(1).StringCellValue;
+
+                    //из всех собранных помещений в моделе ищем помещение с указанным номером
+                    var room = rooms.FirstOrDefault(r => r.Number.Equals(number));
+
+                    //ели помещение такое не найдено, то продолжаем цикл
+                    if (room == null)
+                    {
+                        rowIndex++; //здесь также должны увеличивать rowIndex
+                        continue;
+                    }
+                        
+                    //если все впорядке то используем конструкцию using и создаем транзакцию в которую записываем данные
+                    using(var ts = new Transaction(doc, "Set parameter"))
+                    {
+                        ts.Start();
+                        room.get_Parameter(BuiltInParameter.ROOM_NAME).Set(name);
+                        ts.Commit();
+                    }
+
+                    rowIndex++; //для того, чтобы переходить каждый раз к новой строке
+                }
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ SheetExts.cs
@@ -0,0 +1,51 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIReadingDataFromExcelFile
+{
+    public static class SheetExts
+    {
+        public static void SetCelValue<T>(this ISheet sheet, int rowIndex, int columnIndex, T value)
+        {
+            var cellReference = new CellReference(rowIndex, columnIndex); //указываем ссылку на ячейку
+            var row = sheet.GetRow(cellReference.Row); //создаем новую переменную
+
+            //если строки с таким индексом cellReference.Row нету, то нужно будет ее создать, таким образом создаем новую строку
+            if (row == null)
+                row = sheet.GetRow(cellReference.Row);
+
+            //создаем ссылку на конкретную ячейку, по умолчанию ее тоже может не быть
+            var cell = row.GetCell(cellReference.Col);
+
+            //если ячейка пустая, то создаем ее
+            if (cell == null)
+                cell = row.CreateCell(cellReference.Col);
+
+            //нужно проверить, что является значением, которое нужно будет записать в ячейку
+            //проверяем: если передан сюда текст, то записываем данное значение как текст
+            if (value is string)
+            {
+                cell.SetCellValue((string)(object)value);//делаем преобразование, по скольку у нас значение обобщенное value, по этому
+                                                         //сначала должны преобразовать данное значение в object, а потом уже в string
+            }
+
+            //иначе, если значение у нас double,то в этом случае 
+            else if (value is double)
+            {
+                cell.SetCellValue((double)(object)value);
+            }
+
+            //если значение целочисленное int
+            else if (value is int)
+            {
+                cell.SetCellValue((int)(object)value);
+            }
+
+        }
+    }
+}
